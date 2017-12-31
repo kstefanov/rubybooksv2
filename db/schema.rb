c72b3baecffe83250bd065230533b895b280c928
Kristiyan <kristiqn73@gmail.com>
2017-12-31 15:47:58 +0000

    bug fixes

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,22 @@
 
 ActiveRecord::Schema.define(version: 20171231120750) do
 
+ create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "ar_internal_metadata", ["key"], name: "sqlite_autoindex_ar_internal_metadata_1", unique: true
+
   create_table "books", force: :cascade do |t|
     t.string   "title"
     t.string   "genre"
     t.text     "description"
     t.string   "isbn"
     t.datetime "publish_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
